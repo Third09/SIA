815747f02668dd130241b1f96ad08a52765c63ed
Gelo <168171720+Hyunuki@users.noreply.github.com>
2025-10-05 15:11:34 +0800

    Update Productpage.js

--- frontend/js/Productpage.js
+++ frontend/js/Productpage.js
@@ -1,1 +1,12 @@
+const plus = document.getElementById('plus');
+const minus = document.getElementById('minus');
+const qty = document.getElementById('quantity');
+
+plus.addEventListener('click', () => {
+  qty.value = parseInt(qty.value) + 1;
+});
+
+minus.addEventListener('click', () => {
+  if (parseInt(qty.value) > 1) qty.value = parseInt(qty.value) - 1;
+});
 
